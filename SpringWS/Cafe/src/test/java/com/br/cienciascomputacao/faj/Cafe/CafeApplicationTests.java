fcf14855c26c06800781be98fcba55904412b960
LeoImenes <leonardoimenes@hotmail.com>
2022-09-02 10:20:41 -0300

    att

---
+++ SpringWS/Cafe/src/test/java/com/br/cienciascomputacao/faj/Cafe/CafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.cienciascomputacao.faj.Cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
